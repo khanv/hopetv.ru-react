1dd7092c5edc3b079502d718ac9c4a7b2efdcf8b
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-03-29 14:52:51 +0300

    City component

--- src/components/Map/City/component.js
+++ src/components/Map/City/component.js
@@ -1,7 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-export default function City() {
+const mapWidth = 2000;
+const mapHeight = 1335;
+const mapLonLeft = 22;
+const mapLonRight = 40;
+const mapLonDelta = mapLonRight - mapLonLeft;
+const mapLatBottom = 44.38722;
+const mapLatBottomDegree = mapLatBottom * Math.PI / 180;
+
+function convertGeoToPixel(latitude, longitude) {
+    const x = (longitude - mapLonLeft) * (mapWidth / mapLonDelta);
+
+    latitude = latitude * Math.PI / 180;
+    const worldMapWidth = ((mapWidth / mapLonDelta) * 360) / (2 * Math.PI);
+    const mapOffsetY = (worldMapWidth / 2 * Math.log((1 + Math.sin(mapLatBottomDegree))
+        / (1 - Math.sin(mapLatBottomDegree))));
+    const y = mapHeight - ((worldMapWidth / 2 * Math.log((1 + Math.sin(latitude))
+        / (1 - Math.sin(latitude)))) - mapOffsetY);
+
+    return { x, y };
+}
+
+export default function City(props) {
+    const { lat, lon, children } = props;
+    const { x, y } = convertGeoToPixel(lat, lon);
+
     return (
-        <g/>
+        <g>
+            <circle
+                cx={ x }
+                cy={ y }
+                r="10"
+                fill="#fff"
+            />
+            <circle
+                cx={ x }
+                cy={ y }
+                r="5"
+                fill="#f9c90f"
+            />
+            <text x={ x } y={ y + 40 }>{ children }</text>
+        </g>
     );
 }
+
+City.propTypes = {
+    lat: PropTypes.number.isRequired,
+    lon: PropTypes.number.isRequired,
+    children: PropTypes.string
+};
--- src/components/Map/Region/component.js
+++ src/components/Map/Region/component.js
@@ -4,8 +4,8 @@ import { translate } from 'react-i18next';
 import Styles from './main.scss';
 
 function Region(props) {
-    const { Bubble } = Map;
-    const { path, title, t } = props;
+    const { City, Bubble } = Map;
+    const { path, title, t, cities } = props;
 
     let titleComponent;
     const titleParts = t(title.text).split('-');
@@ -40,11 +40,18 @@ function Region(props) {
         );
     }
 
+    const cityComponents = cities.map((city) => {
+        return (
+            <City { ...city.pos } key={ `city-${city.id}` }>{ city.title }</City>
+        );
+    });
+
     return (
         <g className={ Styles.region }>
             <path d={ path } fill="#DFE1E2"/>
             { titleComponent }
             <Bubble x={ title.pos.x } y={ title.pos.y + 20 }>5</Bubble>
+            <g>{ cityComponents }</g>
         </g>
     );
 }
@@ -52,7 +59,11 @@ function Region(props) {
 Region.propTypes = {
     path: PropTypes.string.isRequired,
     title: PropTypes.object.isRequired,
+    cities: PropTypes.array,
     t: PropTypes.func
+};
+Region.defaultProps = {
+    cities: []
 };
 
 export default translate(['region'])(Region);
