e9806eae6b0a68d80ca309bc033e26ee41f5a53e
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-04-12 16:31:47 +0300

    Html component rewrite to pure function

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -12,57 +12,55 @@ import Helmet from 'react-helmet';
  * HTML doctype declaration, which is added to the rendered output
  * by the server.js file.
  */
-export default class Html extends Component {
-    static propTypes = {
-        assets: PropTypes.object,
-        component: PropTypes.node,
-        store: PropTypes.object
-    };
+export default function Html(props) {
+    const { assets, component, store } = props;
+    const content = component ? ReactDOM.renderToString(component) : '';
+    const head = Helmet.rewind();
 
-    render() {
-        const { assets, component, store } = this.props;
-        const content = component ? ReactDOM.renderToString(component) : '';
-        const head = Helmet.rewind();
+    return (
+        <html lang="ru">
+            <head>
+                { head.base.toComponent() }
+                { head.title.toComponent() }
+                { head.meta.toComponent() }
+                { head.link.toComponent() }
+                { head.script.toComponent() }
 
-        return (
-            <html lang="ru">
-                <head>
-                    { head.base.toComponent() }
-                    { head.title.toComponent() }
-                    { head.meta.toComponent() }
-                    { head.link.toComponent() }
-                    { head.script.toComponent() }
+                <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico"/>
+                <link rel="apple-touch-icon" type="image/png" href="/favicon.png"/>
+                <link
+                    rel="apple-touch-icon"
+                    type="image/png"
+                    href="/favicon-152.png"
+                    sizes="152x152"
+                />
 
-                    <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico"/>
-                    <link rel="apple-touch-icon" type="image/png" href="/favicon.png"/>
+                {/* styles (will be present only in production with webpack extract text plugin) */}
+                { Object.keys(assets.styles).map((style, key) =>
                     <link
-                        rel="apple-touch-icon"
-                        type="image/png"
-                        href="/favicon-152.png"
-                        sizes="152x152"
-                    />
-
-                    {/* styles (will be present only in production with webpack extract text plugin) */}
-                    { Object.keys(assets.styles).map((style, key) =>
-                        <link
-                            href={ assets.styles[style] }
-                            key={ key }
-                            media="screen, projection"
-                            rel="stylesheet"
-                            type="text/css"
-                            charSet="UTF-8"
-                        />
-                    ) }
-                </head>
-                <body>
-                    <div id="content" dangerouslySetInnerHTML={ { __html: content } }/>
-                    <script
-                        dangerouslySetInnerHTML={ { __html: `window.__data=${serialize(store.getState())};` } }
+                        href={ assets.styles[style] }
+                        key={ key }
+                        media="screen, projection"
+                        rel="stylesheet"
+                        type="text/css"
                         charSet="UTF-8"
                     />
-                    <script src={ assets.javascript.main } charSet="UTF-8"/>
-                </body>
-            </html>
-        );
-    }
+                ) }
+            </head>
+            <body>
+                <div id="content" dangerouslySetInnerHTML={ { __html: content } }/>
+                <script
+                    dangerouslySetInnerHTML={ { __html: `window.__data=${serialize(store.getState())};` } }
+                    charSet="UTF-8"
+                />
+                <script src={ assets.javascript.main } charSet="UTF-8"/>
+            </body>
+        </html>
+    );
 }
+
+Html.propTypes = {
+    assets: PropTypes.object,
+    component: PropTypes.node,
+    store: PropTypes.object
+};
