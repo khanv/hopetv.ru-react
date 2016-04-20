cad7f58f5826df0e1179cbc0af03294e6cc78cd0
neomulik <neomulik@hotmail.com>
2016-04-20 16:48:05 +0300

    fixed some bug

--- src/containers/WatchUs/WatchUs.js
+++ src/containers/WatchUs/WatchUs.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Locator } from 'components/WatchUs';
+import { Info, Locator } from 'components/WatchUs';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class WatchUs extends Component {
     render() {
         return (
             <section>
+                <Info/>
                 <Locator/>
             </section>
         );
