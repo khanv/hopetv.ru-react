32ca2bd226ed6c2317538fccdbc91d3ff7fb38bf
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-06-01 00:57:29 +0300

    PageHead update

--- src/components/Shared/PageHead/component.js
+++ src/components/Shared/PageHead/component.js
@@ -3,22 +3,29 @@ import BreakPoints from 'components/PixelPerfect/breakpoints';
 import Styles from './main.scss';
 
 export default function PageHead(props) {
-    const background = 'https://cdn.hope.ua/web/tv.hope.ua/assets/page-background.png';
-    const { mediaType, title, description } = props;
+    const { mediaType, title, description, background } = props;
 
     const scaleFactor = {
         [BreakPoints.phonePortrait.name]: 0.1562500,
         [BreakPoints.phoneLandscape.name]: 0.08803,
         [BreakPoints.tabletPortrait.name]: 0.065106,
-        [BreakPoints.tabletLandscape.name]: 0.08803
+        [BreakPoints.tabletLandscape.name]: 0.08803,
+        [BreakPoints.desktop.name]: 0.08803,
+        [BreakPoints.desktopWide.name]: 0.08803,
+        [BreakPoints.desktopHD.name]: 0.08803,
+        [BreakPoints.desktopMega.name]: 0.08803
     };
     const heightValues = {
         [BreakPoints.phonePortrait.name]: 280,
         [BreakPoints.phoneLandscape.name]: 280,
         [BreakPoints.tabletPortrait.name]: 560,
-        [BreakPoints.tabletLandscape.name]: 222
+        [BreakPoints.tabletLandscape.name]: 222,
+        [BreakPoints.desktop.name]: 222,
+        [BreakPoints.desktopWide.name]: 222,
+        [BreakPoints.desktopHD.name]: 222,
+        [BreakPoints.desktopMega.name]: 222
     };
-    let pageHeadComponentStyles = {
+    const pageHeadComponentStyles = {
         height: `${scaleFactor[mediaType] * heightValues[mediaType]}vw`,
         backgroundImage: `url(${background})`
     };
@@ -41,12 +48,13 @@ export default function PageHead(props) {
 }
 
 PageHead.propTypes = {
-    browser: PropTypes.object.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string,
+    background: PropTypes.string,
     mediaType: PropTypes.string.isRequired
 };
 
-PageHead.defaultTypes = {
-    description: null
+PageHead.defaultProps = {
+    description: null,
+    background: 'https://cdn.hope.ua/web/tv.hope.ua/assets/page-background.png'
 };
