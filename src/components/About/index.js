a0ebc73283370d8cf75799729fb5621f965ff824
AntonGarasko <tonius7570@gmail.com>
2016-06-13 18:25:18 +0300

    add component.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,4 +2,4 @@ export Why from './Why/component';
 export Banner from './Banner/component';
 export WorldNetwork from './WorldNetwork/component';
 export IconInfo from './IconInfo/component';
-export Buttons from './Buttons/component.tpl';
+export Buttons from './Buttons/component';
