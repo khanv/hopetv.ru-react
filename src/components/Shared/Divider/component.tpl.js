be9c104f79e11ea55918ec3e319715ad5e0240c8
CheStas <chstas2@gmail.com>
2016-06-14 13:13:32 +0300

    add Divider component

---
+++ src/components/Shared/Divider/component.tpl.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Styles from './main.scss';
+import PixelPerfect from 'components/PixelPerfect/component';
+import BreakPoints from 'components/PixelPerfect/breakpoints';
+
+export default function Divider() {
+    const templates = [
+        BreakPoints.phonePortrait.name,
+        BreakPoints.phoneLandscape.name,
+        BreakPoints.tabletPortrait.name
+    ];
+
+    return (
+        <PixelPerfect templates={ templates } component="divider">
+            <section className={ Styles.dividerComponent }>
+            </section>
+        </PixelPerfect>
+    );
+}
