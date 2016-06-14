55267faacc0c9b64e9115b46f45baa0e356ba4a3
CheStas <chstas2@gmail.com>
2016-06-14 16:33:53 +0300

    add Divider component(fix lint)

--- src/components/Shared/Divider/component.js
+++ src/components/Shared/Divider/component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Styles from './main.scss';
 
 export default function Divider() {
-	return (
-		<section className={ Styles.dividerComponent }></section>
+    return (
+        <section className={ Styles.dividerComponent }></section>
 	);
 }
