f142ef64da07d38fac70515d3f50d703f4abce7a
a.pavliuk <jenakorj@gmail.com>
2016-06-15 14:24:05 +0300

    banner fix

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 export Why from './Why/component';
-export Banner from './Banner/component';
+export Banner from './Banner/component.tpl';
 export WorldNetwork from './WorldNetwork/component';
 export IconInfo from './IconInfo/component';
 export Buttons from './Buttons/component';
